d709ab46487f93068b0cb0ec5bd4f9672b76648e
Khin yati Sett <khinyatisett5@gmail.com>
2022-09-06 15:40:29 +0630

    Uploading my code

---
+++ homepage.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="inj.css">
+</head>
+<body>
+    <?php
+   
+
+    ?>
+    <h1>University of Computer Studies Taungoo</h1>
+    
+    <div class="tablediv"><table class="table1" cellpadding="30">
+        
+        <tr><td><button name="students" value="students"><a href="http://localhost/injection/studentstable.php"> Shows Students Data</a></button></td><td><button>Graduate Students List</button></td></tr>
+        <tr><td><button>Update Students Data</button></td><td><button>Delete Student Data</button></td></tr>
+
+    </table>
+</div>
+
+</body>
+</html>
